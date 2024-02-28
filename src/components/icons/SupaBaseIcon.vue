0f5735270ab54b519afa26774132296bf3b1a993
Ahmad940 <ahmadmuhammadmak5@gmail.com>
2024-02-28 22:14:35 +0100

    update: Initial commit

--- src/components/icons/SupaBaseIcon.vue
+++ src/components/icons/SupaBaseIcon.vue
@@ -1,5 +1,47 @@
 <template>
-  <svg viewBox="0 0 128 128"></svg>
+  <svg viewBox="0 0 128 128">
+    <defs>
+      <linearGradient
+        id="a"
+        x1="53.974"
+        x2="94.163"
+        y1="54.974"
+        y2="71.829"
+        gradientTransform="translate(29.387 60.096) scale(1.1436)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0" stop-color="#249361"></stop>
+        <stop offset="1" stop-color="#3ecf8e"></stop>
+      </linearGradient>
+      <linearGradient
+        id="b"
+        x1="36.156"
+        x2="54.484"
+        y1="30.578"
+        y2="65.081"
+        gradientTransform="translate(29.387 60.096) scale(1.1436)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0"></stop>
+        <stop offset="1" stop-opacity="0"></stop>
+      </linearGradient>
+    </defs>
+    <path
+      fill="url(#a)"
+      d="M102.24 186.21c-3.267 4.117-9.904 1.862-9.977-3.397l-1.156-76.906h51.715c9.365 0 14.587 10.817 8.763 18.149z"
+      transform="translate(-27.722 -60.338)"
+    ></path>
+    <path
+      fill="url(#b)"
+      fill-opacity=".2"
+      d="M102.24 186.21c-3.267 4.117-9.904 1.862-9.977-3.397l-1.156-76.906h51.715c9.365 0 14.587 10.817 8.763 18.149z"
+      transform="translate(-27.722 -60.338)"
+    ></path>
+    <path
+      fill="#3ecf8e"
+      d="M53.484 2.128c3.267-4.117 9.905-1.862 9.977 3.396l.508 76.907H12.902c-9.365 0-14.587-10.817-8.764-18.149z"
+    ></path>
+  </svg>
 </template>
 
 <script setup lang="ts"></script>
--- src/data/about.data.ts
+++ src/data/about.data.ts
@@ -1,7 +1,9 @@
 export const aboutData = {
   name: 'Ahmad Muhammad',
   role: 'Software Engineer',
-  brief_description: 'I build pixel-perfect, engaging, and accessible digital experiences.',
+  brief_description:
+    'Crafting user-centric digital experiences, I blend innovation and expertise to develop engaging, accessible, and high-quality solutions.',
+  // brief_description: 'I build pixel-perfect, engaging, and accessible digital experiences.',
   description: [
     'My journey into the world of technology began in my childhood when I had access to a family computer at home primary for educational purpose. As I explored the realms of gaming and online entertainment, my curiosity grew, and I became increasingly intrigued by how computer programs were created. This fascination sparked a passion for technology and marked the beginning of my journey into software engineering.',
     "Today, I'm a Software Engineer with a passion for creating innovative solutions. Experienced in front-end and back-end development, Equipped with a diverse and promising skill-set. Proficient in various platforms, languages, Experienced with the latest cutting edge development tools and procedures, proficient at turning ideas into reality. Committed to delivering high-quality code and enhancing user experiences. Seeking opportunities to contribute to challenging projects and collaborate with driven teams."
