8d33f4d7110dc28613d9a2177fb67553b8fb4b3f
Ahmad940 <ahmadmuhammadmak5@gmail.com>
2024-02-29 12:10:44 +0100

    update: Added google analytics

--- src/components/core/FooterSection.vue
+++ src/components/core/FooterSection.vue
@@ -1,7 +1,5 @@
 <template>
-  <footer class="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
-    <p>Inspired and designed By Brittany</p>
-  </footer>
+  <footer class="max-w-md pb-16 text-sm text-slate-500 sm:pb-0"></footer>
 </template>
 
 <script setup lang="ts"></script>
